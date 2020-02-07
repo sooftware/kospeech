0ba4f7df4d1df3718b1f2ac9f707ff844dc7a529
KimSooHwan <42150335+sh951011@users.noreply.github.com>
2020-02-08 05:20:43 +0900

    [project] Implement Beam-Search Ongoing 2

--- models/beam.py
+++ models/beam.py
@@ -30,33 +30,43 @@ class Beam:
         init_step_output = init_predicted_softmax.squeeze(1)
         # top value & top k
         # (batch_size, k)
-        init_topv, init_topk = init_step_output.topk(self.k)
-        self.cumulative_p = init_topv
-        self.candidates = speller_input = init_topk
+        self.cumulative_p, self.candidates = init_step_output.topk(self.k)
+        speller_input = self.candidates
         self.candidates = self.candidates.view(self.batch_size, self.k, 1)
 
-        for di in range(self.max_len):
+        for di in range(self.max_len-1):
             predicted_softmax = self.forward_step(speller_input, listener_outputs)
-            # (batch_size, classfication_num)
+            # (batch_size, k, classfication_num)
+            # 빔 개수만큼의 2,040개에 대한 분포를 구한다.
             step_output = predicted_softmax.squeeze(1)
+            # (batch_size, k, k)
+            # 2,040개의 분포 중 상위 K개를 뽑는다
             candidate_p, candidate_k = step_output.topk(self.k)
-            print(candidate_k.size())
-            print(candidate_k)
-            element_p = (self.cumulative_p * candidate_p).view(self.batch_size, self.k * self.k)
-            select_p, test = element_p.topk(self.k)
-            print(test.size())
-            print(test)
-            select_k = candidate_k.view(self.batch_size, self.k * self.k)
-            self.cumulative_p *= select_p # 확률쪽 여기 수정
-            self.candidates = torch.cat([self.candidates, select_k.view(self.batch_size, self.k, 1)], dim=2)
-            # 이제 추가까지 됐는데 상단 노드랑 어떻게 매칭을 시켜줄 것인가,,,
-
+            # (batch_size, k * k)
+            # K개의 입력에서, K개의 출력이 각각 나왔으므로, K * K 형상으로 바꿔준다.
+            # Ex) 3개의 빔에서 3개의 아웃풋이 나왔다면 결국은, 9개의 확률이 나온 것.
+            # Log-probability라 덧셈으로 !!
+            candidate_p = (self.cumulative_p + candidate_p).view(self.batch_size, self.k * self.k)
+            candidate_k = candidate_k.view(self.batch_size, self.k * self.k)
+            # (batch_size, k)
+            # K * K개 중 K개를 고른다.
+            # Ex) 앞에서 나온 9개의 확률 중 큰 3개 (빔사이즈) 를 고른다.
+            select_p, select_idx = candidate_p.topk(self.k)
+            # (batch_size, k)
+            select_k = torch.LongTensor(self.batch_size, self.k)
+            # (batch_size, k, seq_len)
+            candidates = torch.LongTensor(self.candidates.size(0), self.candidates.size(1), self.candidates.size(2))
+            parent_node = (select_idx % self.k).view(self.batch_size, self.k)
+            for batch_num, batch in enumerate(select_idx):
+                for idx, value in enumerate(batch):
+                    select_k[batch_num, idx] = candidate_k[batch_num, value]
+                    candidates[batch_num, idx] = self.candidates[batch_num, parent_node[batch_num, idx]]
+            self.candidates = torch.cat([candidates, select_k.view(self.batch_size, self.k, 1)], dim=2)
             speller_input = select_k
-            if di > 10:
-                exit()
+            """ eos 처리 & Length Penalty 추가해야함 """
 
     def forward_step(self, speller_input, listener_outputs):
-        output_size = speller_input.size(1)  # speller_input.size(1) : seq_len
+        output_size = speller_input.size(1)
         embedded = self.embedding(speller_input)
         embedded = self.input_dropout(embedded)
 
@@ -68,4 +78,3 @@ class Beam:
         # torch.view()에서 -1이면 나머지 알아서 맞춰줌
         predicted_softmax = self.decode_func(self.out(output.contiguous().view(-1, self.hidden_size)), dim=1).view(self.batch_size,output_size,-1)
         return predicted_softmax
-
