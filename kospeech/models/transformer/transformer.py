39c82a6daeb2853a48ada57d71adf52fd2e68fde
KimSooHwan <sh951011@gmail.com>
2020-07-19 14:48:02 +0900

    Debug

--- kospeech/models/transformer/transformer.py
+++ kospeech/models/transformer/transformer.py
@@ -68,6 +68,7 @@ class Transformer(nn.Module):
         if return_attns:
             return output, encoder_self_attns, decoder_self_attns, memory_attns
 
+        print("End==")
         return output
 
 
