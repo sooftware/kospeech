86383ae6f20f3eb54e7bb88414d7b3e20ec64dde
KimSooHwan <sh951011@gmail.com>
2020-08-21 04:36:11 +0900

    Delete preprocess dir

--- bin/model_architecture.py
+++ bin/model_architecture.py
@@ -1,9 +1,15 @@
 from kospeech.models.acoustic.seq2seq.encoder import SpeechEncoderRNN
 from kospeech.models.acoustic.seq2seq.decoder import SpeechDecoderRNN
 from kospeech.models.acoustic.seq2seq.seq2seq import SpeechSeq2seq
+from kospeech.models.acoustic.transformer.transformer import SpeechTransformer
+
 
 encoder = SpeechEncoderRNN(80, 512, 'cpu')
 decoder = SpeechDecoderRNN(2038, 151, 1024, 1, 2)
 model = SpeechSeq2seq(encoder, decoder)
 
+print(model)
+
+
+model = SpeechTransformer(num_classes=2038, num_encoder_layers=3, num_decoder_layers=3)
 print(model)
--- preprocess/prepare-ksponspeech.sh
+++
@@ -1,6 +0,0 @@
-DATASET_PATH="SET YOUR KsponSpeech corpus PATH"
-NEW_PATH="SET YOUR path to store preprocessed KsponSpeech corpus"
-SCRIPT_PREFIX='KsponScript_'
-
-
-python prepare_ksponspeech.py --dataset_path "$DATASET_PATH" --new_path "$NEW_PATH" --script_prefix $SCRIPT_PREFIX
