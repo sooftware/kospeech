6f7831cb0d7222cfad56149820e5eea8713b0263
sooftware <sh951011@gmail.com>
2020-09-12 18:19:24 +0900

    Fix base hyperparameter

--- run_seq2seq.sh
+++ run_seq2seq.sh
@@ -14,7 +14,7 @@ BATCH_SIZE=32
 NUM_WORKERS=4
 NUM_EPOCHS=20
 HIDDEN_DIM=512
-DROPOUT=0.3
+DROPOUT=0.15
 NUM_HEADS=4
 ATTN_MECHANISM='multi-head'   # multi-head, loc, additive, scaled-dot
 LABEL_SMOOTHING=0.1
@@ -23,16 +23,16 @@ NUM_ENCODER_LAYERS=3
 NUM_DECODER_LAYERS=2
 RNN_TYPE='lstm'
 TEACHER_FORCING_RATIO=1.0
-TEACHER_FORCING_STEP=0.02
-MIN_TEACHER_FORCING_RATIO=0.8
+TEACHER_FORCING_STEP=0.01
+MIN_TEACHER_FORCING_RATIO=0.9
 VALID_RATIO=0.05
-MAX_LEN=120
+MAX_LEN=150
 MAX_GRAD_NORM=400
 WEIGHT_DECAY=1e-05
 INIT_LR=3e-06
 HIGH_PLATEAU_LR=3e-04
 RAMPUP_PERIOD=400
-FRAME_LENGTH=20
+FRAME_LENGTH=25
 FRAME_SHIFT=10
 SAMPLE_RATE=16000
 N_MELS=80
@@ -59,7 +59,7 @@ python ./main.py --batch_size $BATCH_SIZE --num_workers $NUM_WORKERS --num_epoch
 --num_encoder_layers $NUM_ENCODER_LAYERS --num_decoder_layers $NUM_DECODER_LAYERS --rnn_type $RNN_TYPE \
 --high_plateau_lr $HIGH_PLATEAU_LR --teacher_forcing_ratio $TEACHER_FORCING_RATIO --valid_ratio $VALID_RATIO \
 --sample_rate $SAMPLE_RATE --frame_length $FRAME_LENGTH --frame_shift $FRAME_SHIFT --n_mels $N_MELS --normalize --del_silence \
---feature_extract_by $FEATURE_EXTRACT_BY --time_mask_para $TIME_MASK_PARA --freq_mask_para $FREQ_MASK_PARA \
+--feature_extract_by $FEATURE_EXTRACT_BY --freq_mask_para $FREQ_MASK_PARA \
 --time_mask_num $TIME_MASK_NUM --freq_mask_num $FREQ_MASK_NUM --save_result_every $SAVE_RESULT_EVERY \
 --checkpoint_every $CHECKPOINT_EVERY --print_every $PRINT_EVERY --init_lr $INIT_LR  \
 --mode $MODE --dataset_path $DATASET_PATH --data_list_path $DATA_LIST_PATH  \
