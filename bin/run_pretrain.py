468465e9bc32d5d5455c650df83638f1dd1d47b9
sooftware <sh951011@gmail.com>
2020-11-06 01:44:05 +0900

    run_pretrain.py fix parse_audio()

--- bin/run_pretrain.py
+++ bin/run_pretrain.py
@@ -1,8 +1,9 @@
 import argparse
 import torch
-import torchaudio
+import librosa
 import sys
 sys.path.append('..')
+from kospeech.vocab import KsponSpeechVocabulary
 from torch import Tensor
 from kospeech.data.audio.core import load_audio
 from kospeech.model_builder import load_test_model
@@ -10,14 +11,12 @@ from kospeech.model_builder import load_test_model
 
 def parse_audio(audio_path: str, del_silence: bool = True) -> Tensor:
     signal = load_audio(audio_path, del_silence)
+    mfcc = librosa.feature.mfcc(y=signal, sr=16000, n_mfcc=40, n_fft=320, hop_length=160)
 
-    feature_vector = torchaudio.compliance.kaldi.fbank(Tensor(signal).unsqueeze(0), num_mel_bins=80,
-                                                       frame_length=20, frame_shift=10,
-                                                       window_type='hamming').transpose(0, 1).numpy()
-    feature_vector -= feature_vector.mean()
-    feature_vector = Tensor(feature_vector).transpose(0, 1)
+    mfcc -= mfcc.mean()
+    mfcc = Tensor(mfcc).transpose(0, 1)
 
-    return feature_vector
+    return mfcc
 
 
 parser = argparse.ArgumentParser(description='Run Pretrain')
@@ -28,7 +27,7 @@ opt = parser.parse_args()
 
 feature_vector = parse_audio(opt.audio_path, del_silence=True)
 input_length = torch.IntTensor([len(feature_vector)])
-vocab = KsponSpeechVocabulary()
+vocab = KsponSpeechVocabulary('../data/vocab/aihub_vocabs.csv')
 
 model = load_test_model(opt, opt.device)
 model.eval()
