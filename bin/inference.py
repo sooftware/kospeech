7f717bb13717881eeb6746b8a33f4ac9045343dc
Soohwan Kim <sh951011@gmail.com>
2020-12-19 04:40:48 +0900

    Update inference.py

--- bin/inference.py
+++ bin/inference.py
@@ -28,27 +28,29 @@ def parse_audio(audio_path: str, del_silence: bool = False, audio_extension: str
     return torch.FloatTensor(feature_vector).transpose(0, 1)
 
 
-model_path = 'MODEL_PATH'
-audio_path = 'AUDIO_PATH'
-device = 'cpu'
+parser = argparse.ArgumentParser(description='KoSpeech')
+parser.add_argument('--model_path', type=str, require=True)
+parser.add_argument('--audio_path', type=str, require=True)
+parser.add_argument('--device', type=str, require=False, default='cpu')
+opt = parser.parse_args()
 
-feature_vector = parse_audio(audio_path, del_silence=True)
+feature_vector = parse_audio(opt.audio_path, del_silence=True)
 input_length = torch.IntTensor([len(feature_vector)])
 vocab = KsponSpeechVocabulary('../data/vocab/aihub_character_vocabs.csv')
 
-model = torch.load(model_path, map_location=lambda storage, loc: storage).to(device)
+model = torch.load(opt.model_path, map_location=lambda storage, loc: storage).to(opt.device)
 if isinstance(model, nn.DataParallel):
     model = model.module
 model.eval()
 
 if isinstance(model, ListenAttendSpell):
-    model.encoder.device = device
-    model.decoder.device = device
+    model.encoder.device = opt.device
+    model.decoder.device = opt.device
 
-    y_hats = model.greedy_decode(feature_vector.unsqueeze(0), input_length, device)
+    y_hats = model.greedy_decode(feature_vector.unsqueeze(0), input_length, opt.device)
 elif isinstance(model, DeepSpeech2):
-    model.device = device
-    y_hats = model.greedy_decode(feature_vector.unsqueeze(0), input_length, device)
+    model.device = dopt.evice
+    y_hats = model.greedy_decode(feature_vector.unsqueeze(0), input_length, opt.device)
 
 sentence = vocab.label_to_string(y_hats.cpu().detach().numpy())
 print(sentence)
