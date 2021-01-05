c383866a02b7f7602966d2f99a375b8223dd0ec3
sooftware <sh951011@gmail.com>
2021-01-05 20:18:43 +0900

    Update

--- kospeech/utils.py
+++ kospeech/utils.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Soohwan Kim, Seyoung Bae, Cheolhwang Won.
 # @ArXiv : KoSpeech: Open-Source Toolkit for End-to-End Korean Speech Recognition
 # This source code is licensed under the Apache 2.0 License license found in the
