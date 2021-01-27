c9edaf1e6664a86277a7dad0dfdf39f09d90584f
sanghoon <57610448+shleee47@users.noreply.github.com>
2021-01-27 16:25:36 +0900

    Update modules.py

--- kospeech/models/modules.py
+++ kospeech/models/modules.py
@@ -18,7 +18,20 @@ import torch.nn.functional as F
 import torch.nn.init as init
 
 from torch import Tensor
-from typing import Tuple
+
+class ResidualConnectionModule(nn.Module):
+    """
+    Residual Connection Module.
+    outputs = (module(inputs) x module_factor + inputs x input_factor)
+    """
+    def __init__(self, module: nn.Module, module_factor: float = 1.0, input_factor: float = 1.0):
+        super(ResidualConnectionModule, self).__init__()
+        self.module = module
+        self.module_factor = module_factor
+        self.input_factor = input_factor
+
+    def forward(self, inputs: Tensor) -> Tensor:
+        return (self.module(inputs) * self.module_factor) + (inputs * self.input_factor)
 
 
 class BaseRNN(nn.Module):
