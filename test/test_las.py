969de03b27471349fd314a2a9d19c563ab1275f6
sanghoon <57610448+shleee47@users.noreply.github.com>
2021-01-31 23:47:11 +0900

    Update test_las.py

--- test/test_las.py
+++ test/test_las.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 import torch
-
+from kospeech.criterion import JointCTCCrossEntropyLoss
 from kospeech.models import ListenAttendSpell
 from kospeech.models.las.encoder import EncoderRNN
 from kospeech.models.las.decoder import DecoderRNN
@@ -22,17 +22,16 @@ B, T, D, H = 3, 12345, 80, 32
 
 cuda = torch.cuda.is_available()
 device = torch.device('cuda' if cuda else 'cpu')
+criterion = JointCTCCrossEntropyLoss(num_classes=10, ignore_index=0, reduction='mean')
 
 inputs = torch.rand(B, T, D).to(device)
 input_lengths = torch.IntTensor([T, T - 100, T - 1000])
-targets = torch.LongTensor([[1, 1, 2], [3, 4, 2], [7, 2, 0]]).to(device)
+targets = torch.LongTensor([[1, 1, 2], [1, 4, 2], [1, 2, 0]]).to(device)
+target_lengths = torch.LongTensor([2, 2, 1])
 
-encoder = EncoderRNN(input_dim=D, hidden_state_dim=H, joint_ctc_attention=False)
+encoder = EncoderRNN(input_dim=D, hidden_state_dim=H, joint_ctc_attention=True, num_classes=10)
 decoder = DecoderRNN(num_classes=10, hidden_state_dim=H << 1, max_length=10)
 model = ListenAttendSpell(encoder, decoder).to(device)
 
-model(inputs, input_lengths, targets, teacher_forcing_ratio=0.0)
-print("teacher_forcing_ratio=0.0 PASS")
-
-model(inputs, input_lengths, targets, teacher_forcing_ratio=1.0)
-print("teacher_forcing_ratio=1.0 PASS")
+predicted_log_probs, encoder_log_probs, output_lengths = model(inputs, input_lengths, targets, teacher_forcing_ratio=1.0)
+outputs = torch.stack(predicted_log_probs, dim=1).to(device)
