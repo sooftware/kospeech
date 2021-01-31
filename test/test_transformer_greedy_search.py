5bdf8a628fd2fb2918faf0e5c892a5b4a18e7d32
sanghoon <57610448+shleee47@users.noreply.github.com>
2021-01-31 20:54:50 +0900

    Update test_transformer_greedy_search.py

--- test/test_transformer_greedy_search.py
+++ test/test_transformer_greedy_search.py
@@ -24,5 +24,5 @@ transformer = SpeechTransformer(num_classes=10, d_model=16, d_ff=32, num_encoder
 inputs = torch.FloatTensor(batch_size, seq_length, input_size)
 input_lengths = torch.LongTensor([seq_length, seq_length - 10, seq_length - 20, seq_length - 30])
 
-output = transformer.greedy_search(inputs, input_lengths, device='cuda')
+output = transformer.recognize(inputs, input_lengths)
 print(output)
