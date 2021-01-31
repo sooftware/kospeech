ff90c01817153bad722de2bf7bc02f0a90f84f37
sanghoon <57610448+shleee47@users.noreply.github.com>
2021-02-01 00:35:40 +0900

    Update test_conformer.py

--- test/test_conformer.py
+++ test/test_conformer.py
@@ -8,15 +8,28 @@ batch_size, sequence_length, dim = 3, 12345, 80
 cuda = torch.cuda.is_available()
 device = torch.device('cuda' if cuda else 'cpu')
 
-inputs = torch.rand(batch_size, sequence_length, dim).to(device)
-input_lengths = torch.IntTensor([12345, 12300, 12000])
 
 model = nn.DataParallel(Conformer(
     num_classes=10,
     input_dim=dim,
-    encoder_dim=512,
+    encoder_dim=32,
     num_layers=3,
     device=device,
 )).to(device)
 
-outputs, output_lengths = model(inputs, input_lengths) 
+criterion = nn.CTCLoss(blank=3, zero_infinity=True)
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-04)
+
+for i in range(10):
+    inputs = torch.rand(batch_size, sequence_length, dim).to(device)
+    input_lengths = torch.IntTensor([12345, 12300, 12000])
+    targets = torch.LongTensor([[1, 3, 3, 3, 3, 3, 4, 5, 6, 2],
+                                [1, 3, 3, 3, 3, 3, 4, 5, 2, 0],
+                                [1, 3, 3, 3, 3, 3, 4, 2, 0, 0]]).to(device)
+    target_lengths = torch.LongTensor([9, 8, 7])
+
+    outputs, output_lengths = model(inputs, input_lengths)
+    loss = criterion(outputs.transpose(0, 1), targets[:, 1:], output_lengths, target_lengths)
+    loss.backward()
+    optimizer.step()
+    print(loss)
