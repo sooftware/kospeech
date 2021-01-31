5da2e35815b20a33f0018d4db2c6654a08a80fae
sanghoon <57610448+shleee47@users.noreply.github.com>
2021-01-31 20:50:23 +0900

    Create test_conformer.py

---
+++ test/test_conformer.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+
+from kospeech.models.conformer import Conformer
+
+batch_size, sequence_length, dim = 3, 12345, 80
+
+cuda = torch.cuda.is_available()
+device = torch.device('cuda' if cuda else 'cpu')
+
+inputs = torch.rand(batch_size, sequence_length, dim).to(device)
+input_lengths = torch.IntTensor([12345, 12300, 12000])
+
+model = nn.DataParallel(Conformer(
+    num_classes=10,
+    input_dim=dim,
+    encoder_dim=512,
+    num_layers=3,
+    device=device,
+)).to(device)
+
+outputs, output_lengths = model(inputs, input_lengths) 
