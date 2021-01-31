17b76657a608b2d54d1a0028868f6875bb4f08de
sanghoon <57610448+shleee47@users.noreply.github.com>
2021-01-31 20:51:44 +0900

    Create test_las_decoder.py

---
+++ test/test_las_decoder.py
@@ -0,0 +1,30 @@
+# Copyright (c) 2021, Soohwan Kim. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import torch
+from kospeech.models.las.decoder import Speller
+
+cuda = torch.cuda.is_available()
+device = torch.device('cuda' if cuda else 'cpu')
+
+inputs = torch.LongTensor([[1, 1, 2], [3, 4, 2], [7, 2, 0]])
+encoder_outputs = torch.rand(3, 100, 32)
+
+decoder = Speller(num_classes=10, hidden_state_dim=32, max_length=10, device=device)
+decoder_outputs = decoder(inputs, encoder_outputs, teacher_forcing_ratio=1.0)
+print("teacher_forcing_ratio=1.0 PASS")
+
+decoder = Speller(num_classes=10, hidden_state_dim=32, max_length=10, device=device)
+decoder_outputs = decoder(inputs, encoder_outputs, teacher_forcing_ratio=0.0)
+print("teacher_forcing_ratio=0.0 PASS")
