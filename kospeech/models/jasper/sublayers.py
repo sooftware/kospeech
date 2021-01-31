b01fef4a13f8af7cf51c8b580838d95ffa321f90
sanghoon <57610448+shleee47@users.noreply.github.com>
2021-01-31 20:17:26 +0900

    Update sublayers.py

--- kospeech/models/jasper/sublayers.py
+++ kospeech/models/jasper/sublayers.py
@@ -19,72 +19,6 @@ from torch import Tensor
 from kospeech.models.conv import MaskConv1d
 
 
-class JasperBlock(nn.Module):
-    """
-    Jasper Block: The Jasper Block consists of R Jasper sub-block.
-
-    Args:
-        num_sub_blocks (int): number of sub block
-        in_channels (int): number of channels in the input feature
-        out_channels (int): number of channels produced by the convolution
-        kernel_size (int): size of the convolving kernel
-        stride (int): stride of the convolution. (default: 1)
-        dilation (int): spacing between kernel elements. (default: 1)
-        bias (bool): if True, adds a learnable bias to the output. (default: True)
-        dropout_p (float): probability of dropout
-        activation (str): activation function
-
-    Inputs: inputs, input_lengths, residual
-        - **inputs**: tensor contains input sequence vector
-        - **input_lengths**: tensor contains sequence lengths
-        - **residual**: tensor contains residual vector
-
-    Returns: output, output_lengths
-        - **output**: tensor contains output sequence vector
-        - **output**: tensor contains output sequence lengths
-    """
-    def __init__(
-            self,
-            num_sub_blocks: int,
-            in_channels: int,
-            out_channels: int,
-            kernel_size: int,
-            stride: int = 1,
-            dilation: int = 1,
-            bias: bool = True,
-            dropout_p: float = 0.2,
-            activation: str = 'relu',
-    ) -> None:
-        super(JasperBlock, self).__init__()
-        padding = self.get_same_padding(kernel_size, stride, dilation)
-        self.layers = nn.ModuleList([
-            JasperSubBlock(
-                in_channels=in_channels if i == 0 else out_channels,
-                out_channels=out_channels,
-                kernel_size=kernel_size,
-                stride=stride,
-                dilation=dilation,
-                padding=padding,
-                bias=bias,
-                dropout_p=dropout_p,
-                activation=activation,
-            ) for i in range(num_sub_blocks)
-        ])
-
-    def get_same_padding(self, kernel_size: int, stride: int, dilation: int):
-        if stride > 1 and dilation > 1:
-            raise ValueError("Only stride OR dilation may be greater than 1")
-        return (kernel_size // 2) * dilation
-
-    def forward(self, inputs: Tensor, input_lengths: Tensor, residual: Tensor) -> Tuple[Tensor, Tensor]:
-        for layer in self.layers[:-1]:
-            inputs, input_lengths = layer(inputs, input_lengths)
-
-        outputs, output_lengths = self.layers[-1](inputs, input_lengths, residual)
-
-        return outputs, output_lengths
-
-
 class JasperSubBlock(nn.Module):
     """
     Jasper sub-block applies the following operations: a 1D-convolution, batch norm, ReLU, and dropout.
@@ -114,9 +48,9 @@ class JasperSubBlock(nn.Module):
         'relu': nn.ReLU(inplace=True),
         'elu': nn.ELU(inplace=True),
         'leaky_relu': nn.LeakyReLU(inplace=True),
-        'gelu': nn.GELU()
+        'gelu': nn.GELU(),
     }
-
+    
     def __init__(
             self,
             in_channels: int,
@@ -143,7 +77,7 @@ class JasperSubBlock(nn.Module):
         self.batch_norm = nn.BatchNorm1d(out_channels, eps=1e-3, momentum=0.1)
         self.activation = self.supported_activations[activation]
         self.dropout = nn.Dropout(p=dropout_p)
-
+        
     def forward(self, inputs: Tensor, input_lengths: Tensor, residual: Optional[Tensor] = None) -> Tuple[Tensor, Tensor]:
         outputs, output_lengths = self.conv(inputs, input_lengths)
         outputs = self.batch_norm(outputs)
@@ -152,6 +86,72 @@ class JasperSubBlock(nn.Module):
             outputs += residual
 
         outputs = self.dropout(self.activation(outputs))
-        del inputs, input_lengths, residual
+
+        return outputs, output_lengths
+
+
+class JasperBlock(nn.Module):
+    """
+    Jasper Block: The Jasper Block consists of R Jasper sub-block.
+
+    Args:
+        num_sub_blocks (int): number of sub block
+        in_channels (int): number of channels in the input feature
+        out_channels (int): number of channels produced by the convolution
+        kernel_size (int): size of the convolving kernel
+        stride (int): stride of the convolution. (default: 1)
+        dilation (int): spacing between kernel elements. (default: 1)
+        bias (bool): if True, adds a learnable bias to the output. (default: True)
+        dropout_p (float): probability of dropout
+        activation (str): activation function
+
+    Inputs: inputs, input_lengths, residual
+        - **inputs**: tensor contains input sequence vector
+        - **input_lengths**: tensor contains sequence lengths
+        - **residual**: tensor contains residual vector
+
+    Returns: output, output_lengths
+        - **output**: tensor contains output sequence vector
+        - **output**: tensor contains output sequence lengths
+    """
+
+    def __init__(
+            self,
+            num_sub_blocks: int,
+            in_channels: int,
+            out_channels: int,
+            kernel_size: int,
+            stride: int = 1,
+            dilation: int = 1,
+            bias: bool = True,
+            dropout_p: float = 0.2,
+            activation: str = 'relu',
+    ) -> None:
+        super(JasperBlock, self).__init__()
+        padding = self._get_same_padding(kernel_size, stride, dilation)
+        self.layers = nn.ModuleList([
+            JasperSubBlock(
+                in_channels=in_channels if i == 0 else out_channels,
+                out_channels=out_channels,
+                kernel_size=kernel_size,
+                stride=stride,
+                dilation=dilation,
+                padding=padding,
+                bias=bias,
+                dropout_p=dropout_p,
+                activation=activation,
+            ) for i in range(num_sub_blocks)
+        ])
+
+    def _get_same_padding(self, kernel_size: int, stride: int, dilation: int):
+        if stride > 1 and dilation > 1:
+            raise ValueError("Only stride OR dilation may be greater than 1")
+        return (kernel_size // 2) * dilation
+
+    def forward(self, inputs: Tensor, input_lengths: Tensor, residual: Tensor) -> Tuple[Tensor, Tensor]:
+        for layer in self.layers[:-1]:
+            inputs, input_lengths = layer(inputs, input_lengths)
+
+        outputs, output_lengths = self.layers[-1](inputs, input_lengths, residual)
 
         return outputs, output_lengths
